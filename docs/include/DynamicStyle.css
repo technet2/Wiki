7e554d8f5974b2e19cf47b4e714e8f3d0fc08451
Peter Laker <peterlaker@hotmail.com>
2019-02-17 20:48:07 +0000

    includes

---
+++ docs/include/DynamicStyle.css
@@ -0,0 +1,44 @@
+
+
+
+
+.content-fragment { margin: 10px; }
+
+.layout-content.content-left-sidebar-right .layout-region.content .content-fragment,
+.layout-content.sidebar-left-content-right .layout-region.left-sidebar .content-fragment,
+.layout-content.sidebar-left-content-center-sidebar-right .layout-region.left-sidebar .content-fragment,
+.layout-content.sidebar-left-content-center-sidebar-right .layout-region.content .content-fragment,
+.layout-content.content-left-split-sidebar-right-top-single-sidebar-right-bottom .layout-region.content .content-fragment,
+.layout-content.content-left-split-sidebar-right-top-single-sidebar-right-bottom .layout-region.split-sidebar-left .content-fragment,
+.layout-content.header-top-content-left-sidebar-right .layout-region.content .content-fragment,
+.layout-content.header-top-sidebar-left-content-right .layout-region.left-sidebar .content-fragment,
+.layout-content.header-top-sidebar-left-content-center-sidebar-right .layout-region.left-sidebar .content-fragment,
+.layout-content.header-top-sidebar-left-content-center-sidebar-right .layout-region.content .content-fragment,
+.layout-content.header-top-content-left-split-sidebar-right-top-single-sidebar-right-bottom .layout-region.content .content-fragment,
+.layout-content.header-top-content-left-split-sidebar-right-top-single-sidebar-right-bottom .layout-region.split-sidebar-left .content-fragment
+{ margin-right: 0; }
+                                
+.layout-region.header .content-fragment { margin-right: 0; margin-left: 0; }
+
+.layout-content.split-content .layout-region.content .content-fragment,
+.layout-content.header-top-split-content .layout-region.content .content-fragment
+{ margin-right: 5px; }
+
+.layout-content.split-content .layout-region.right-sidebar .content-fragment,
+.layout-content.header-top-split-content .layout-region.right-sidebar .content-fragment
+{ margin-left: 5px; }
+
+.content-fragment.no-wrapper { margin: 0; }
+.content-fragment.side-border-only { margin-top: 0; margin-bottom: 0; }
+.content-fragment.top-side-border-only { margin-bottom: 0; }
+.content-fragment.side-bottom-border-only { margin-top: 0; }
+
+li.field-item.post-feature {display:none}
+
+
+.content-fragment-page.postlist .content-fragment.comment-list .content-list {margin-bottom: 105px;}
+
+#megabladeContainer .bladeItemMenu h5 {font-size: 2.15em;}
+
+.MSTToolbarVisible .header-fragment-outer.fiji-header-fragment-outer.site-banner {top: 80px;}
+.MSTToolbarVisible #megabladeContainer {top: 40px;}
